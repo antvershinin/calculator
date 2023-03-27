5b127ff36b4c1365941f0ac06f14fd26f11dd8c4
Anton Vershinin <a.vershinin92@gmail.com>
2023-03-27 16:26:22 +0300

    added arrays

--- script.js
+++ script.js
@@ -2,76 +2,63 @@
 
 const screen = document.querySelector(".calculator__screen");
 const buttons = document.querySelector(".calculator__buttons");
-const numbersKeyboard = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-const actionsKeyboard = ["*", "/", "^", "+", "-"];
+const numbersKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const actionsKeys = ["*", "/", "^", "+", "-"];
 
-const operations = {
-  "*"(a, b) {
-    return +a * +b;
-  },
-  "/"(a, b) {
-    return +a / +b;
-  },
-  "+"(a, b) {
-    return +a + +b;
-  },
-  "-"(a, b) {
-    return +a - +b;
-  },
-  "^"(a, b) {
-    return a ** +b;
-  },
-  "√"(a) {
-    return Math.sqrt(a);
-  },
-};
-
-let a = "";
-let b = "";
-let action = "";
+let operands = [""];
+let newOperandIndex;
 let completed = false;
 
 function enterNumber(target) {
   if (completed) {
-    a = "";
-    b = "";
-    action = "";
+    operands = [""];
     screen.textContent = "0";
   }
-  if (b === "" && action === "") {
+  if (operands.length === 1) {
     completed = false;
-    target !== "." ? (a += target) : a.includes(".") ? null : (a += target);
-    a = a.length > 10 ? a.slice(0, -1) : a;
-    screen.textContent = a;
+    target !== "."
+      ? (operands[0] += target)
+      : operands[0].includes(".")
+      ? null
+      : (operands[0] += target);
+    // a = a.length > 10 ? a.slice(0, -1) : a;
+    screen.textContent = operands[0];
   } else {
-    target !== "." ? (b += target) : b.includes(".") ? null : (b += target);
-    b = b.length > 10 ? b.slice(0, -1) : b;
-    screen.textContent = b;
+    target !== "."
+      ? (operands[newOperandIndex] += target)
+      : operands[operands.length].includes(".")
+      ? null
+      : (operands[newOperandIndex] += target);
+    // b = b.length > 10 ? b.slice(0, -1) : b;
   }
+  console.log(operands);
 }
 
-function calculate(target) {
-  a = target === "√" ? operations["√"](a) : operations[`${action}`](a, b);
-  screen.textContent = a;
+function calculate(operands) {
+  operands = operands.map((el) =>
+    !actionsKeys.includes(el) && el !== "" ? +el : Symbol(el)
+  );
+
   completed = true;
 }
 
 function addAction(target) {
   completed = false;
-  b = "";
-  action = target;
-  screen.textContent = action;
+  operands[operands.length] = target;
+  newOperandIndex = operands.length;
+  operands.push("");
+  console.log(operands);
 }
 
-buttons.addEventListener("keypress", (e) => {
-  if (numbersKeyboard.includes(e.key)) {
-    enterNumber(e.key);
-  } else if (actionsKeyboard.includes(e.key)) {
-    addAction(e.key);
-  } else if (e.key === "=") {
-    calculate(e.key);
-  } else return;
-});
+// buttons.addEventListener("keypress", (e) => {
+//   if (numbersKeyboard.includes(e.key)) {
+//     enterNumber(e.key);
+//   } else if (actionsKeyboard.includes(e.key)) {
+//     addAction(e.key);
+//   } else if (e.key === "=") {
+//     calculate(e.key);
+//   } else return;
+// });
 
 buttons.addEventListener("click", (e) => {
   if (!e.target.classList.contains("button")) return;
@@ -90,35 +77,31 @@ buttons.addEventListener("click", (e) => {
 
     case "equal":
       {
-        calculate(e.target.textContent);
+        calculate(operands);
       }
 
       break;
 
-    case "clear":
-      {
-        a = "";
-        b = "";
-        action = "";
-        screen.textContent = "0";
-      }
+      // case "clear":
+      //   {
+      //     a = "";
+      //     b = "";
+      //     action = "";
+      //     screen.textContent = "0";
+      //   }
 
-      break;
+      //   break;
 
-    case "action--delete":
-      {
-        if (a.includes(".")) {
-          a = a.slice(0, -1);
-          a = a.endsWith(".") ? a.slice(0, -1) : a;
-          screen.textContent = a;
-        }
-        console.log(a);
-      }
+      // case "action--delete":
+      //   {
+      //     if (a.includes(".")) {
+      //       a = a.slice(0, -1);
+      //       a = a.endsWith(".") ? a.slice(0, -1) : a;
+      //       screen.textContent = a;
+      //     }
+      //     console.log(a);
+      //   }
 
       break;
   }
-  console.log("a:" + a);
-  console.log("b:" + b);
-  console.log("action:" + action);
-  console.log("-----------");
 });
