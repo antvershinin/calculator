de418ce7108fadc29e1e59decdfb9596e247c432
Anton Vershinin <a.vershinin92@gmail.com>
2023-03-29 09:58:13 +0300

    ...

--- script.js
+++ script.js
@@ -1,18 +1,84 @@
 "use strict";
 
-const screen = document.querySelector(".calculator__screen");
+const screenMain = document.querySelector(".screen__main");
+const screenExpression = document.querySelector(".screen__expression");
 const buttons = document.querySelector(".calculator__buttons");
 const numbersKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const actionsKeys = ["*", "/", "^", "+", "-"];
 
 let operands = [""];
 let newOperandIndex;
+let currentOperand;
 let completed = false;
+let result = "";
+let stackNumbers = [];
+let stackOperators = [];
+let currentOperator;
+
+const priority = {
+  "*": 2,
+  "/": 2,
+  "+": 1,
+  "-": 1,
+};
+
+const operations = {
+  "*": (a, b) => a * b,
+  "/": (a, b) => b / a,
+  "+": (a, b) => a + b,
+  "-": (a, b) => b - a,
+};
+
+function calculate(array) {
+  array = array.map((el) =>
+    !actionsKeys.includes(el) && el !== "" ? +el : el
+  );
+
+  function makeCalc() {
+    stackNumbers.push(
+      operations[stackOperators.pop()](stackNumbers.pop(), stackNumbers.pop())
+    );
+  }
+
+  array.forEach((el) => {
+    switch (typeof el) {
+      case "number":
+        stackNumbers.push(el);
+
+        break;
+
+      case "string":
+        if (
+          priority[el] > priority[stackOperators[stackOperators.length - 1]] ||
+          stackOperators.length === 0
+        )
+          stackOperators.push(el);
+        else if (
+          priority[el] <= priority[stackOperators[stackOperators.length - 1]]
+        ) {
+          do {
+            makeCalc();
+            currentOperator = stackOperators[stackOperators.length - 1];
+          } while (priority[el] <= priority[currentOperator]);
+          stackOperators.push(el);
+        }
+    }
+  });
+
+  if (stackOperators.length === 2) makeCalc();
+  if (stackOperators.length === 1) makeCalc();
+
+  screenMain.textContent = result = stackNumbers[0];
+  completed = true;
+  operands = [""];
+}
 
 function enterNumber(target) {
   if (completed) {
     operands = [""];
-    screen.textContent = "0";
+    stackNumbers = [];
+    screenMain.textContent = "0";
+    screenExpression.textContent = "0";
   }
   if (operands.length === 1) {
     completed = false;
@@ -22,24 +88,18 @@ function enterNumber(target) {
       ? null
       : (operands[0] += target);
     // a = a.length > 10 ? a.slice(0, -1) : a;
-    screen.textContent = operands[0];
+    screenMain.textContent = operands[0];
   } else {
     target !== "."
       ? (operands[newOperandIndex] += target)
       : operands[operands.length].includes(".")
       ? null
       : (operands[newOperandIndex] += target);
+    screenMain.textContent = operands[newOperandIndex];
     // b = b.length > 10 ? b.slice(0, -1) : b;
   }
+  screenExpression.textContent = operands.join(" ");
   console.log(operands);
-}
-
-function calculate(operands) {
-  operands = operands.map((el) =>
-    !actionsKeys.includes(el) && el !== "" ? +el : Symbol(el)
-  );
-
-  completed = true;
 }
 
 function addAction(target) {
@@ -65,13 +125,13 @@ buttons.addEventListener("click", (e) => {
 
   switch (e.target.id) {
     case "number":
-      enterNumber(e.target.textContent);
+      enterNumber(e.target.value);
 
       break;
 
     case "action":
       {
-        addAction(e.target.textContent);
+        addAction(e.target.value);
       }
       break;
 
@@ -82,15 +142,8 @@ buttons.addEventListener("click", (e) => {
 
       break;
 
-      // case "clear":
-      //   {
-      //     a = "";
-      //     b = "";
-      //     action = "";
-      //     screen.textContent = "0";
-      //   }
-
-      //   break;
+    case "clear":
+      break;
 
       // case "action--delete":
       //   {
